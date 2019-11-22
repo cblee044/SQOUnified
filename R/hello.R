560544312c4d79cb0dc32b623af408e578092970
Paul Smith <pauls@sccwrp.org>
2019-11-22 10:49:15 -0800

    setup - first commit

---
+++ R/hello.R
@@ -0,0 +1,42 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+sqo <- function(itr='all',ifile='db') {
+  # itr = index to run - by default we run all indices
+  # ifile = input file - not required - if not supplied we call b18 db
+  exists(itr)
+    print(itr)
+  exists(ifile)
+    print(ifile)
+  print("start sqo processing")
+
+  # call MAMBI - bring in current dataframe could be existing db records or user input file
+
+  # call RBI
+
+  # output to excel
+
+
+
+
+}
+
+
+
+
+
+
+
+
