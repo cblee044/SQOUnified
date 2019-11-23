26e3e25731bc18e14b2d539b155ca40ce0190c1b
GIS Computer <GisUser@SCCWRP2k.lan>
2019-11-22 19:13:17 -0800

    Main function: SQOUnified is the name of the main function. It takes in a database and the desired index scores. It returns a csv file with the scores. Note that right now it only computes the MAMBI scores. We need to add to this file once the other scores are implemented so that they are added to the final output. When we are done with this project, this function will take in a list of all the index scores the user wants.

---
+++ R/SQOUnified.R
@@ -0,0 +1,55 @@
+"benthic_data"
+#' Compute the SQO index scores.
+#'
+#' @param DB A data file string name that we want to compute SQO scores for.
+#' @param SQO A list of the type of SQO scores that we want to compute
+#'     (e.g., \code{c("MAMBI", "RBI")}).
+#'     The default is \code{"all"}, meaning that all scores will be computed.
+#' @usage data(benthic_data)
+#' @examples
+#' SQOUnified(DB = benthic_data, SQO = "all")
+#' SQOUnified(DB = benthic_data, SQO = "MAMBI")
+#' SQOUnified(DB = benthic_data, SQO = c("MAMBI", "RBI")) --> To be implemented in
+#' the future
+
+
+
+SQOUnified <- function(DB = benthic_data, SQO = "all"){
+
+  # Compute ALL SQO scores
+  if (SQO == "all"){
+    mambi.score <- MAMBI(DB, EG_File_Name="data/Ref - EG Values 2018.csv", EG_Scheme="Hybrid")
+    final.scores <- mambi.score # will add other scores to this data frame as they are computed
+  } else {
+    mambi.score <- MAMBI(DB, EG_File_Name="data/Ref - EG Values 2018.csv", EG_Scheme="Hybrid")
+    final.scores <- mambi.score # will add other scores to this data frame as they are computed
+  }
+
+  ####
+  # Will create a for loop with the other indices to make sure that all wanted indices are given
+  # in the final csv file. For now, we just have the MAMBI score (see "else" condition above).
+  ####
+
+  # # Compute RBI scores
+  # elseif (SQO == "RBI"){
+  #   mambi.score <- MAMBI(DB, EG_File_Name="data/Ref - EG Values 2018.csv", EG_Scheme="Hybrid")
+  # }
+  #
+  # # Compute IBI scores
+  # elseif (SQO == "IBI"){
+  #   mambi.score <- MAMBI(DB, EG_File_Name="data/Ref - EG Values 2018.csv", EG_Scheme="Hybrid")
+  # }
+  #
+  # # Compute BRI scores
+  # elseif (SQO == "BRI"){
+  #   mambi.score <- MAMBI(DB, EG_File_Name="data/Ref - EG Values 2018.csv", EG_Scheme="Hybrid")
+  # }
+  #
+  # # Compute RIVPACS score
+  # elseif (SQO == "RIVPACS"){
+  #   mambi.score <- MAMBI(DB, EG_File_Name="data/Ref - EG Values 2018.csv", EG_Scheme="Hybrid")
+  # }
+
+  # OUTPUT: Write an xlsx file to current working directory
+  write.csv(mambi.score, file = "SQO-Unified.csv", row.names = FALSE)
+}
