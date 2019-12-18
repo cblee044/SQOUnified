fa3d6552aec2f4b5ca6d5c22391712e62679b147
Robert Butler <r7butler@yahoo.com>
2019-12-18 08:39:30 -0800

    Done with a rough draft of the R code that does the same thing as the query titled 'BRI - 1'

--- R/BRI.R
+++ R/BRI.R
@@ -30,6 +30,16 @@ con <- DBI::dbConnect(
 infauna <- tbl(con, "tbl_infaunalabundance_initial") %>% as_tibble()
 assignment <- tbl(con, "field_assignment_table") %>% as_tibble()
 
-BRI1 <- infauna %>% dplyr::left_join(Taxonomic_Info, by = c('taxon' = 'Taxon'))
+BRI1 <- infauna %>%
+  dplyr::left_join(Taxonomic_Info, by = c('taxon' = 'Taxon')) %>%
+  dplyr::right_join(assignment, by = 'stationid') %>%
+  # I assume that the next line is something they had in there as a method of removing duplicates
+  # for this reason, this next line will likely be eliminated.
+  # They grouped by all the columns that were selected (In query BRI - 1)
+  # Instead, if need be we can use something from dplyr that deals with duplicates
+  dplyr::group_by(stratum, stationid, replicate, taxon, abundance, `B-CodeScore`) %>%
+  dplyr::filter(stratum %in% c("Estuaries", "Marinas", "Bays", "Ports")) %>%
+  dplyr::filter(!is.na(`B-CodeScore`)) %>%
+  dplyr::select(stratum, stationid, replicate, taxon, abundance, `B-CodeScore`)
 
 
