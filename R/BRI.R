b16ff712f5f471bf8f3f4e86a74793ea854e5a54
Robert Butler <r7butler@yahoo.com>
2019-12-18 17:41:11 -0800

    cleaning up BRI.R

--- R/BRI.R
+++ R/BRI.R
@@ -4,33 +4,20 @@
 #' @usage data(benthic_data)
 #' @usage data(EG_Ref)
 #' @usage data(Taxonomic_Info)
+#' @usage data(assignment)
 #'
 #' @examples
-#' MAMBI.DJG.alt(benthic_data, EG_File_Name="data/Ref - EG Values 2018.csv", EG_Scheme="Hybrid")
-#' MAMBI.DJG.alt(benthic_data, EG_File_Name="data/Ref - EG Values 2018.csv", EG_Scheme="US_Gulf")
+#'
+#'
+"assignment"
+"benthic_data"
+"EG_Ref"
+"Taxonomic_Info"
 
 
-library(DBI) # needed to connect to database
-library(dbplyr) # needed to connect to database
-library(RPostgreSQL) # needed to connect to our database
-library(rstudioapi) # just so we can type the password as we run the script, so it is not written in the clear
-library(tidyverse)
-
-# con is short for connection
-# Create connection to the database
-con <- DBI::dbConnect(
-  PostgreSQL(),
-  host = "192.168.1.16",
-  dbname = 'bight2018',
-  user = 'b18read',
-  password = '1969$Harbor' # if we post to github, we might want to do rstudioapi::askForPassword()
-)
-
-
-infauna <- tbl(con, "tbl_infaunalabundance_initial") %>% as_tibble()
-assignment <- tbl(con, "field_assignment_table") %>% as_tibble()
-
-BRI <- infauna %>%
+BRI <- function(BenthicData)
+{
+  out <- BenthicData %>%
   dplyr::left_join(Taxonomic_Info, by = c('taxon' = 'Taxon')) %>%
   dplyr::right_join(assignment, by = 'stationid') %>%
   # I assume that the next line is something they had in there as a method of removing duplicates
@@ -55,6 +42,7 @@ BRI <- infauna %>%
     BRI_Score = sum(tolerance_score, na.rm = T) / sum(fourthroot_abun, na.rm = T)
   )
 
-
+  return(out)
+}
 
 
