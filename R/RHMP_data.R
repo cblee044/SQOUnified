f0f5bb45e1cea1dc46e47ed46034b94796d2a16e
GIS Computer <GisUser@SCCWRP2k.lan>
2020-01-24 13:19:15 -0800

    A script to run the SQOUnified function. This script imports the RHMP station IDs and filters through the current data

---
+++ R/RHMP_data.R
@@ -0,0 +1,14 @@
+# create a vector with all the RHMP stations of interest
+# Note: We pasted the stations into a text file and created a csv.
+#       This csv file is what is read in.
+rhmp = read.csv("RHMP_data.csv", fileEncoding="UTF-8-BOM", header = FALSE)
+
+# stations are read in as factors. We coerce them into characters.
+rhmp = as.character(rhmp$V1)
+
+# filter the data base qeuery by the RHMP stations of interest
+DB = test %>%
+  dplyr::filter(StationID %in% rhmp)
+
+# Run the SQOUnified code to get the calculated benthic indices
+
