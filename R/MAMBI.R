35ff0f6628622e3506279b68913002c98b07dc49
Robert Butler <r7butler@yahoo.com>
2020-04-06 15:14:56 -0700

    MAMBI now uses the RData file EG_Ref.RData

--- R/MAMBI.R
+++ R/MAMBI.R
@@ -80,14 +80,14 @@
 
 
 
-MAMBI<-function(BenthicData, EG_File_Name="data/Ref - EG Values 2018.csv", EG_Scheme="Hybrid")
+MAMBI<-function(BenthicData, EG_Ref = EG_Ref, EG_Scheme="Hybrid")
 {
   require(tidyverse)
   require(reshape2)
   require(vegan)
   require(readxl)
   #source("EQR.R")
-  #"EG_Ref"
+  "EG_Ref"
   "benthic_data"
   "Saline_Standards"
   "TidalFresh_Standards"
@@ -106,7 +106,13 @@ MAMBI<-function(BenthicData, EG_File_Name="data/Ref - EG Values 2018.csv", EG_Sc
 
 
 
-  EG_Ref<-read.csv(EG_File_Name, stringsAsFactors = F, na.strings = "") %>% select(.,Taxon, Exclude, EG=EG_Scheme) %>% mutate(EG=(ifelse(Taxon=="Oligochaeta", "V", EG)))
+  EG_Ref <- EG_Ref %>%
+    select(Taxon, Exclude, EG=EG_Scheme) %>%
+    mutate(
+      EG = ifelse(
+        Taxon=="Oligochaeta", "V", EG
+      )
+    )
   #EG_Ref <- EG_Ref %>% select(.,Taxon, Exclude, EG=EG_Scheme) %>% mutate(EG=(ifelse(Taxon=="Oligochaeta", "V", EG)))
 
   #TODO: Need to fix these lines!!!
